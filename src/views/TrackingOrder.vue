b8db91546275dd3815b2660cc9c48192a8a77d6f
RizkyNurMuzhaki22 <rizki.nurmuzhaki@gmail.com>
2023-08-30 08:41:18 +0700

    update

--- src/views/TrackingOrder.vue
+++ src/views/TrackingOrder.vue
@@ -39,8 +39,8 @@
   <div class="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
     <div class="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
       <div class="flex flex-col justify-start items-start dark:bg-gray-800 bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
-        <p class="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">Customer’s Cart</p>
-        <div v-for="cart in getCart" :key="cart.cart_id" class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
+        <p class="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">Informasi Pesanan :</p>
+        <div class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full" v-for="cart in getCart" :key="cart.cart_id">
           <div class="pb-4 md:pb-8 w-full md:w-40">
             <img class="w-full hidden md:block" src="https://i.ibb.co/84qQR4p/Rectangle-10.png" alt="dress" />
             <img class="w-full md:hidden" src="https://i.ibb.co/L039qbN/Rectangle-10.png" alt="dress" />
@@ -66,9 +66,9 @@
         <div class="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
           <h3 class="text-xl dark:text-white font-semibold leading-5 text-gray-800">Summary</h3>
           <div class="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
-            <div class="flex justify-between w-full">
+            <div class="flex justify-between w-full" v-for="(order) in getOrder.orders[0].products.data">
               <p class="text-base dark:text-white leading-4 text-gray-800">Qty :</p>
-              <p class="text-base dark:text-gray-300 leading-4 text-gray-600">3</p>
+              <p class="text-base dark:text-gray-300 leading-4 text-gray-600">{{ order.quantity }}</p>
             </div>
             <div class="flex justify-between items-center w-full">
               <p class="text-base dark:text-white leading-4 text-gray-800">Diskon<span class="bg-gray-200 p-1 text-xs font-medium dark:bg-white dark:text-gray-800 leading-3 text-gray-800">Promo</span></p>
